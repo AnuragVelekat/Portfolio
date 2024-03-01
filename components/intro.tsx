57dffd820efd5bd6f46d374702310046cf012464
Anurag Velekat <92157150+AnuragVelekat@users.noreply.github.com>
2024-03-01 16:00:24 +0530

    Slight modifications to text

--- components/intro.tsx
+++ components/intro.tsx
@@ -54,7 +54,7 @@ function Intro() {
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}>
-        <span className="font-bold">Hello, I&apos;m Anurag.</span> I&apos;m a{" "}
+        <span className="font-bold">Hello, I&apos;m Anurag,</span> a{" "}
         <span className="font-bold">Software Developer</span> with a passion for{" "}
         <span className="font-bold">AI & Machine Learning.</span>
         <br />
